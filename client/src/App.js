e4cd4a95e71caedbd2f416ec37e1b13485e106db
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-05-09 11:17:38 -0400

    imports in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Welcome from './components/Welcome';
+import Login from "./components/Login"
+import Signup from './components/Signup'
+import DocLogin from './components/DocLogin'
+import { Routes, Route, useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Welcome />
+      <Login />
+      <Signup />
+      <DocLogin />
+    </>
   );
 }
 
