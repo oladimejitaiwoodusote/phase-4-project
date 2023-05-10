bc2063df5e3a79cbe415c930dc1d5e40da7ca01b
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-05-10 11:52:52 -0400

    app.py

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,80 @@ import { Routes, Route, useNavigate } from "react-router-dom"
 import { useState } from "react"
 
 function App() {
+
+  const [currentOwner, setCurrentOwner] = useState(null)
+  const [currentDoctor, setCurrentDoctor] = useState(null)
+
+  // useEffect(()=>{
+  //   fetch('/check_session')
+  //   .then(res => {
+
+  //   })
+  // })
+
+  function attemptLoginOwner(ownerData){
+    fetch('/ownerlogin',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify(ownerData)
+    })
+    .then(response => {
+      if (response.ok){
+        response.json()
+        .then( data => setCurrentOwner(data))
+      }
+      else {
+        response.json()
+        .then( data => alert(data.message))
+      }
+    })
+  }
+
+  function attemptLoginDoctor(doctorData){
+    fetch('/doctorlogin',{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify(doctorData)
+    })
+    .then(response => {
+      if (response.ok) {
+        response.json()
+        .then(data => setCurrentDoctor(data))
+      }
+      else {
+        response.json()
+        .then(data => alert(data.message))
+      }
+    })
+  }
+
+  function attemptSignup(ownerData){
+    fetch('/ownercreate',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify(ownerData)
+    })
+    .then(response => response.json())
+    .then(data => setCurrentOwner(data))
+  }
+
   return (
     <>
       <Routes>
         <Route path ='/' element ={<Welcome/>}/>
-        <Route path ='login' element ={<Login/>}/>
-        <Route path ='signup' element ={<Signup/>}/>
-        <Route path ='doclogin' element ={<DocLogin/>}/>
-      </Routes>
-      
+        <Route path ='login' element ={<Login attemptLogin={attemptLoginOwner}/>}/>
+        <Route path ='signup' element ={<Signup attemptSignup={attemptSignup}/>}/>
+        <Route path ='doclogin' element ={<DocLogin attemptLogin={attemptLoginDoctor}/>}/>
+      </Routes>    
     </>
   );
 }
